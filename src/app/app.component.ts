8fc8fffa9e12fc4f90f9cbc05a4857a4181bb58a
sarairatni <ms_iratni@esi.dz>
2024-12-23 15:54:23 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, UserComponent],
+  template: `
+    <div class="app-container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'eCare';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./user/user.component').then((m) => m.UserComponent);
+    },
+  },
+  { path: 'user', component: UserComponent },
+  { path: '**', component: NotFoundComponent },
+];
