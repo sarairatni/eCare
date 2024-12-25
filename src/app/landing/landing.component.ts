3df7b8c9cb2ea2bd1de9696034b4a347d3dbdb29
sarairatni <ms_iratni@esi.dz>
2024-12-25 08:30:48 +0100

    section five in landing  ready+ responsive/sara

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -4,10 +4,11 @@ import { SectionOneComponent } from "./section-one/section-one.component";
 import { SectionTwoComponent } from './section-two/section-two.component';
 import { SectionThreeComponent } from './section-three/section-three.component';
 import { SectionFourComponent } from './section-four/section-four.component';
+import { SectionFiveComponent } from "./section-five/section-five.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [HeaderComponent, SectionOneComponent,SectionTwoComponent,SectionThreeComponent,SectionFourComponent],
+  imports: [HeaderComponent, SectionOneComponent, SectionTwoComponent, SectionThreeComponent, SectionFourComponent, SectionFiveComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
---
+++ src/app/landing/section-five/section-five.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-five',
+  imports: [],
+  templateUrl: './section-five.component.html',
+  styleUrl: './section-five.component.css'
+})
+export class SectionFiveComponent {
+
+}
