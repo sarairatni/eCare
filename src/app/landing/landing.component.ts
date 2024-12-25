fca05dc84b53e01c669ca4c1f541b592248bafda
sarairatni <ms_iratni@esi.dz>
2024-12-25 09:26:39 +0100

    footer-landing page completed/sara

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,10 +5,11 @@ import { SectionTwoComponent } from './section-two/section-two.component';
 import { SectionThreeComponent } from './section-three/section-three.component';
 import { SectionFourComponent } from './section-four/section-four.component';
 import { SectionFiveComponent } from "./section-five/section-five.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [HeaderComponent, SectionOneComponent, SectionTwoComponent, SectionThreeComponent, SectionFourComponent, SectionFiveComponent],
+  imports: [HeaderComponent, SectionOneComponent, SectionTwoComponent, SectionThreeComponent, SectionFourComponent, SectionFiveComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
