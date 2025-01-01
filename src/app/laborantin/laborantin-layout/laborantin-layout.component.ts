5152bf824ecb9f5e9a3e9019ac8e76c5b005f2fd
Younes <my_saidji@esi.dz>
2025-01-01 14:32:19 +0000

    merged laborantin

---
+++ src/app/laborantin/laborantin-layout/laborantin-layout.component.ts
@@ -0,0 +1,34 @@
+import { Component, input, signal } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { TopbarComponent } from '../../components/topbar/topbar.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-laborantin-layout',
+  imports: [
+    RouterModule,
+    CommonModule,
+    SidebarComponent,
+    TopbarComponent,
+    RouterOutlet,
+  ],
+  templateUrl: './laborantin-layout.component.html',
+  styleUrl: './laborantin-layout.component.css',
+})
+export class LaborantinLayoutComponent {
+  selectionne = signal(0);
+  personne = input('Ladoul Mahdi');
+  menuItems = signal([
+    {
+      text: 'Ajouter analyse',
+      iconUrl: '/icons/albums.svg',
+      route: '/laborantin/ajouter-analyse',
+    },
+    {
+      text: 'Mon profil',
+      iconUrl: '/profil.svg',
+      route: '/doctor/mon-profil',
+    },
+  ]);
+}
