f5b1c25eda6472076c8efe7b009a00ee7af16430
sarairatni <ms_iratni@esi.dz>
2025-01-02 05:14:42 +0100

    ,ouvelle consultation/prescrir un bilan radio ou bio done/sara

--- src/app/doctor/dossier-patient/consultation-bilan/consultation-bilan.component.ts
+++ src/app/doctor/dossier-patient/consultation-bilan/consultation-bilan.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-consultation-bilan',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './consultation-bilan.component.html',
-  styleUrl: './consultation-bilan.component.css'
+  styleUrls: ['./consultation-bilan.component.css'],
 })
-export class ConsultationBilanComponent {
+export class ConsultationBilanComponent implements OnInit {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
+  nss: string = '';
+  type: string = 'Bilan biologique'; // Default value
+  params = [
+    {
+      nom: 'Glycémie',
+      valeur_normale: '0,7 - 1,1 g/L',
+    },
+    {
+      nom: 'Niveaux de cholestérol',
+      valeur_normale: 'Normal: < 2 g/L',
+    },
+    {
+      nom: 'Pression artérielle',
+      valeur_normale: 'Normal: 120/80 mmHg',
+    },
+  ];
+
+  ngOnInit(): void {
+    // Get the nss parameter from the route
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.nss = params.get('nss') || ''; // Store the nss parameter
+    });
+  }
+
+  // Navigate to antecedents page
+  navigateToAntecedants() {
+    this.router.navigate([
+      `/doctor/mes-patients/${this.nss}/nouvelle-consultation/antecedants`,
+    ]);
+  }
+
+  // Delete a parameter from the list
+  deleteParam(index: number) {
+    this.params.splice(index, 1);
+  }
 }
